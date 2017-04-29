4268cbb9c49dfda714a439410337862cc63a6d8c
Bazinga612 <Bazinga612@users.noreply.github.com>
2017-04-28 20:50:31 -0700

    script for having a user dialogue for each of the topics for 15 turns using each of the strategies as mentioned in the file

--- dialogue_log.py
+++ dialogue_log.py
@@ -1,7 +1,18 @@
-filename = name + date + converstion_num
-
-for turn in range(n):
-
-
-
-input()
+import json
+import codecs
+import random
+#list2 includes set of strategies for each topic
+file = open("list2.txt", 'r') 
+strategies_dict = {}
+for line in file: 
+    d2 = json.load(codecs.open(line, "r",encoding='utf-8', errors='ignore'))
+    for key, value in d2.items():
+        strategies_dict[key]=value
+    print("topic is about harry potter!")
+for strategy in strategies_dict.keys():
+    print("this is for strategy: "+strategy)
+    for turns in range(0,15):
+        input('Enter your dialogue: ')
+        action=random.choice(strategies_dict[strategy])
+        print action
+        
