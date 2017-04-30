c2e1feadd50971cad9c9185cb7bf19fc5445ee1c
Molly Zhang <mollyzhang@ucsc.edu>
2017-04-29 17:14:18 -0700

    various bug fixes

--- log_dialogue.py
+++ log_dialogue.py
@@ -3,37 +3,45 @@ from pprint import pprint as pp
 import datetime
 import glob
 import random
+import os
 
 N_TURN = 10
 
 def main():
-
     # load strategies from pickle
     strats = pickle.load(open("strategies.pkl", "rb"))
 
-    # create file
-    name = input("Before we start: what's your name? (Molly, Wei or Nehal)?\n")
-    date = str(datetime.datetime.today().date())
-    convo_num = get_convo_num(name)
-    f = open("./data/{0}_{1}_{2}.txt".format(name.lower(), date, convo_num), "w")
-    
-    # record converstaion
-    strategy = random.choice(list(strats.keys()))
-    print("--------------------here it gones--------------------------------")
-    log("Bot", random.choice(strats[strategy]), strategy, f)
-    for turn in range(N_TURN-1):
-        log(name, input(), "None", f)
-        strategy = random.choice(list(strats.keys()))
-        log("Bot", random.choice(strats[strategy]), strategy, f)
-    log(name, input(), "None", f)
-
-    # evaluation and wrap up
     try:
-        score = int(input("\nHow is the conveseration? (number between 0 to 5) "))
+        # create file
+        name = input("Before we start: what's your name? (Molly, Wei or Nehal)?\n")
+        date = str(datetime.datetime.today().date())
+        convo_num = get_convo_num(name)
+        filename = "./data/{0}_{1}_{2}.txt".format(name.lower(), date, convo_num) 
+        f = open(filename, "w")
+    
+        # record converstaion
+        strategy = random.choice(list(strats.keys()))
+        print("--------------------here it gones--------------------------------")
+        log("Bot", random.choice(strats[strategy]), strategy, f)
+        for turn in range(N_TURN-1):
+            log(name, input(name + ": "), "None", f)
+            strategy = random.choice(list(strats.keys()))
+            log("Bot", random.choice(strats[strategy]), strategy, f)
+        log(name, input(name + ": "), "None", f)
+
+        # evaluation and wrap up
+        score = input("\nHow is the conveseration? (number between 0 to 5) ")
+        while ord(score) < ord("0") or ord(score) > ord("5"):
+            score = input("Type a integer in [0,1,2,3,4,5], otherwise I will keep asking")
+        f.write("evaluation={0}".format(score))
+        f.close()
+        print("Done!")
+    
+    except KeyboardInterrupt:
+        os.remove(filename)
+        print("key board interupted, nothing is recorded")
     except:
-        score = int(input("Type a number between 0-5, otherwise you'll have to type everything all over again"))
-    f.write("evaluation={0}".format(score))
-    f.close()
+        raise    
 
 
 def get_convo_num(name):
